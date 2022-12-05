480f778e8ad7e64c15545ff9d217df7d0bffde66
gauerla <97123043+gauerla@users.noreply.github.com>
2022-12-05 09:00:25 +0100

    neue änderungen im views

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from website import create_app
 
-# we can import it because website is a python package
+
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, ssl_context='adhoc')
+    app.run(debug=True)
 
