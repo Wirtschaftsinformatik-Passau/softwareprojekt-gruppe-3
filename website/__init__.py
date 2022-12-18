d55102dcc73d0205dbcc77e943401bfa8c9be7aa
gauerla <97123043+gauerla@users.noreply.github.com>
2022-12-18 11:39:25 +0100

    flugstatus und buchung suchen

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 
-conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format("root", "", "localhost", "airlinedb")
+conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format("clara", "1234", "localhost", "airline")
 db = SQLAlchemy()
 
 
@@ -17,6 +18,15 @@ def create_app():
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    from .models import Nutzerkonto
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.anmelden"  # if the user is not logged in then he will be directed to login page
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(nutzerid):
+        return Nutzerkonto.query.get(int(nutzerid))
 
     from .models import Flughafen, Flugzeug, Nutzerkonto
 
