3b19a5ff239110c6422f2c77c82b5bd4465c1dcf
Meriem Abbassi <abbass01@ads.uni-passau.de>
2022-12-25 00:36:52 +0100

    passwort vergessen

--- website/__init__.py
+++ website/__init__.py
@@ -1,15 +1,23 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
+import random
 
 conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format("merie", "1234", "localhost", "airline")
 db = SQLAlchemy()
+mail = Mail()
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hdfsoanfi sfnkosnfmeu'
     app.config['SQLALCHEMY_DATABASE_URI'] = conn
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USERNAME'] = 'airpassau.de@gmail.com'
+    app.config['MAIL_PASSWORD'] = 'xelxozlfzphverxt'
     db.init_app(app)
 
     from .nutzer_ohne_account_views import nutzer_ohne_account_views
@@ -26,6 +34,7 @@ def create_app():
     login_manager = LoginManager()
     login_manager.login_view = "auth.anmelden"  # if the user is not logged in then he will be directed to login page
     login_manager.init_app(app)
+    mail.init_app(app)
 
     @login_manager.user_loader
     def load_user(id):
