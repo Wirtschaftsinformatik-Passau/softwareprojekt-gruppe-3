4bb810b3552b357cd8294ce8638ff81149c4da5f
Meriem Abbassi <abbass01@ads.uni-passau.de>
2023-02-06 12:53:37 +0100

    REST API

--- website/controller/flight_api_controller.py
+++ website/controller/flight_api_controller.py
@@ -13,31 +13,34 @@ def get_flights():
     anzahl_passagier=request.args.get('anzahl_passagier')
 
     # Kennung des Ankunftflughafens
-    ankunft_flughafen = Flughafen.query.filter_by(Flughafen.kennung == von).first()
+    ankunft_flughafen = Flughafen.query.filter_by(kennung = von).first()
     # Kennung des Zielflughafens
-    ziel_flughafen = Flughafen.query.filter_by(Flughafen.kennung == nach).first()
+    ziel_flughafen = Flughafen.query.filter_by(kennung = nach).first()
 
     if not ziel_flughafen:
         return jsonify(
-            {'error': 'Der Flughafen {} wird von uns leider nicht angeflogen'.format(nach)}), 400
+            {'error': 'Der Flughafen {} wird von uns leider nicht angeflogen.'.format(nach)}), 400
 
     if not anzahl_passagier or anzahl_passagier <= 0:
-        return jsonify({'error': 'Die Anzahl der Passagiere muss kann nicht null sein.'}), 400
+        return jsonify({'error': 'Die Anzahl der Passagiere kann nicht null sein.'}), 400
 
     if not von or not nach or not datum or not anzahl_passagier:
         return jsonify({'error': 'fehlende Parameter'}), 400
 
-    datum = datetime.strptime(datum, '%Y-%m-%d')
-    today = datetime.now().date()
+    try:
+        datum = datetime.strptime(datum, '%d.%m.%Y')
+    except ValueError:
+        return jsonify({'error': 'Die Datumseingabe muss dem Format "TT.MM.JJ" entsprechen'}), 400
 
-    if datum < today:
+    today = datetime.now().date()
+    if datum.date() < today:
         return jsonify({'error': 'Bitte geben Sie ein Datum ein, welches in der Zukunft liegt.'}), 400
 
     flug = Flug.query.filter(Flug.abflugid == ankunft_flughafen.flughafenid,
                       Flug.zielid == ziel_flughafen.flughafenid,
                       Flug.sollabflugzeit.date() == datum).first()
     if not flug:
-        return jsonify({'error': 'Der Flug existiert nicht.'}), 404
+        return jsonify({'error': 'Zu Ihren Suchkriterien wurde kein passender Flug gefunden'}), 404
 
     results = []
     results.append({
